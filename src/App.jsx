b234736d67aa570f30bee00f859bb76b311f37a4
JosephYin4 Yin <yincc147@hotmail.com>
2024-11-13 07:32:45 +0000

    minor updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
-import './index.css';
 import Navbar from './Navbar';
 import HomePage from './HomePage';
-import ProductPage from './ProductPage';
+import ProductsPage from './ProductPage';
 import RegisterPage from './RegisterPage';
-//import ProductCard from './ProductCard';
 import { Route, Switch } from 'wouter';
 
 function App() {
-return(
-<>
-
-<Navbar />
-<Switch>
-    <Route path="/" component={HomePage} />
-    <Route path="/products" component={ProductPage} />
-    <Route path="/register" component={RegisterPage} />
-</Switch>
-
-<footer className="bg-dark text-white text-center py-3">
-<div className="container">
-  <p>&copy; 2023 E-Shop. All rights reserved.</p>
-</div>
-</footer>
-</>
-);
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route path="/products" component={ProductsPage} />
+        <Route path="/register" component={RegisterPage} />
+      </Switch>
+      <footer className="bg-dark text-white text-center py-3">
+       <div className="container">
+          <p>&copy; 2023 E-Shop. All rights reserved.</p>
+        </div>
+      </footer>
+    </>
+  );
 }
-
-
 
 export default App;
